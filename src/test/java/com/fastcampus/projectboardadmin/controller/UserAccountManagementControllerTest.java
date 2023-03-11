5182f92f12450d4788bca9fbb6e74f468277d1b7
conny <jmg41490400@gmail.com>
2023-03-11 15:37:35 +0900

    #8 - 뷰 컨트롤러, 기본 뷰, 테스트 기초 작성
    어드민에서 필요로 한 뷰는 다음 5가지
    
    * 메인 루트 뷰
    * 게시글 관리 뷰
    * 댓글 관리 뷰
    * 회원 관리 뷰
    * 어드민 회원 뷰
    
    이에 각 뷰를 담당할 컨트롤러 클래스와
    기본적인 최소한의 핸들러 메소드, 뷰 템플릿,
    정상적인 뷰 응답 케이스를 간단히 정의하는 컨트롤러 테스트 작성
    테스트만 통과하는 정도의 구현이고
    실제 비즈니스 로직 구현을 담고 있지 않음

---
+++ src/test/java/com/fastcampus/projectboardadmin/controller/UserAccountManagementControllerTest.java
@@ -0,0 +1,40 @@
+package com.fastcampus.projectboardadmin.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import com.fastcampus.projectboardadmin.config.SecurityConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@DisplayName("View 컨트롤러 - 회원 관리")
+@Import(SecurityConfig.class)
+@WebMvcTest(UserAccountManagementController.class)
+class UserAccountManagementControllerTest {
+
+    private final MockMvc mvc;
+
+    public UserAccountManagementControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view][GET] 회원 관리 페이지 - 정상 호출")
+    @Test
+    void givenNothing_whenRequestingUserAccountManagementView_thenReturn() throws Exception {
+        // given
+
+        // when
+        mvc.perform(get("/management/user-accounts"))
+           .andExpect(status().isOk())
+           .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+           .andExpect(view().name("management/user-accounts"));
+        // then
+    }
+}
