ec9c8fb44ba624eb2c4196d74a536c3660104386
conny <jmg41490400@gmail.com>
2023-03-23 16:16:05 +0900

    #37 - 댓글 관리 페이지의 컨트롤러, 서비스, 뷰 구현

--- src/main/java/com/fastcampus/projectboardadmin/controller/ArticleCommentManagementController.java
+++ src/main/java/com/fastcampus/projectboardadmin/controller/ArticleCommentManagementController.java
@@ -1,23 +1,43 @@
 package com.fastcampus.projectboardadmin.controller;
 
-import static org.springframework.data.domain.Sort.Direction;
-
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fastcampus.projectboardadmin.dto.response.ArticleCommentResponse;
+import com.fastcampus.projectboardadmin.service.ArticleCommentManagementService;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequestMapping("/management/article-comments")
+@RequiredArgsConstructor
 public class ArticleCommentManagementController {
+	private final ArticleCommentManagementService articleCommentManagementService;
 
-    @GetMapping
-    public String articleComments(
-        @PageableDefault(size = 10, sort = "createdAt", direction = Direction.DESC) Pageable pageable,
-        Model model
-    ) {
-        return "management/article-comments";
-    }
+	@GetMapping
+	public String articleComments(Model model) {
+		model.addAttribute(
+			"comments",
+			articleCommentManagementService.getArticleComments().stream().map(ArticleCommentResponse::of).toList()
+		);
+		return "management/article-comments";
+	}
+
+	@ResponseBody
+	@GetMapping("/{articleCommentId}")
+	public ArticleCommentResponse articleComment(@PathVariable Long articleCommentId) {
+		return ArticleCommentResponse.of(articleCommentManagementService.getArticleComment(articleCommentId));
+	}
+
+	@PostMapping("/{articleCommentId}")
+	public String deleteArticleComment(@PathVariable Long articleCommentId) {
+		articleCommentManagementService.deleteArticleComment(articleCommentId);
+
+		return "redirect:/management/article-comments";
+	}
 }
