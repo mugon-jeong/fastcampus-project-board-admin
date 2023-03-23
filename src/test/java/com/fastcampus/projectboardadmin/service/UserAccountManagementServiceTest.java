20c7ee1eecdce091fb16f30bcdceb717c90aa62a
conny <jmg41490400@gmail.com>
2023-03-23 16:38:11 +0900

    #39 - 회원 관리 기능 컨트롤러, 서비스, 뷰 구현

--- src/test/java/com/fastcampus/projectboardadmin/service/UserAccountManagementServiceTest.java
+++ src/test/java/com/fastcampus/projectboardadmin/service/UserAccountManagementServiceTest.java
@@ -1,9 +1,10 @@
 package com.fastcampus.projectboardadmin.service;
 
-import com.fastcampus.projectboardadmin.dto.UserAccountDto;
-import com.fastcampus.projectboardadmin.dto.properties.ProjectProperties;
-import com.fastcampus.projectboardadmin.dto.response.UserAccountClientResponse;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import static org.assertj.core.api.Assertions.*;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.*;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.*;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
@@ -19,12 +20,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.client.MockRestServiceServer;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+import com.fastcampus.projectboardadmin.dto.UserAccountDto;
+import com.fastcampus.projectboardadmin.dto.properties.ProjectProperties;
+import com.fastcampus.projectboardadmin.dto.response.UserAccountClientResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @ActiveProfiles("test")
 @DisplayName("비즈니스 로직 - 회원 관리")
@@ -39,7 +38,7 @@ class UserAccountManagementServiceTest {
 		);
 	}
 
-	//    @Disabled("실제 API 호출 결과 관찰용이므로 평상시엔 비활성화")
+	@Disabled("실제 API 호출 결과 관찰용이므로 평상시엔 비활성화")
 	@DisplayName("실제 API 호출 테스트")
 	@SpringBootTest
 	@Nested
